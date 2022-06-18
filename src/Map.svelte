f3d698607ba797a7cf5d5d56243b39fc31e6fa64
Jolle Carlestam <jolle.carlestam@sogeti.com>
2022-06-18 13:21:41 +0200

    Added store

--- src/Map.svelte
+++ src/Map.svelte
@@ -3,13 +3,30 @@
   let map;
   let zoom = 12;
   let center = { lat: 57.696639517983165, lng: 11.828236342523105 };
-  
+
   import { onMount } from "svelte";
+  import { presentCenter, presentPosition } from "./components/Store/Stores.js";
 
   onMount(async () => {
     map = new google.maps.Map(container, {
       zoom,
       center,
+    });
+    presentCenter.subscribe((pos) => {
+      console.log("pos", pos);
+    });
+    google.maps.event.addListener(map, "click", function (event) {
+      presentCenter.update(() => ({
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      }));
+      alert(
+        "Latitude: " +
+          event.latLng.lat() +
+          " " +
+          ", longitude: " +
+          event.latLng.lng()
+      );
     });
   });
 </script>
