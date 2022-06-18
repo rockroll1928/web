68e261c5b4b47c7cc7af07b2844583d62a470aec
Maximilian Wendel <maximilian.wendel@sogeti.se>
2022-06-18 23:30:47 +0200

    Updated radius for situation API

--- src/services/InfoService.js
+++ src/services/InfoService.js
@@ -2,7 +2,7 @@ const Service = require("./Service");
 
 const InfoService = function (service = new Service()) {
 	const _service = service;
-  InfoService.prototype.getPinList = function (location, radius = 10 * 1000) {
+  InfoService.prototype.getPinList = function (location, radius = 25 * 1000) {
     return _service.get("https://europe-west1-bootcamp222.cloudfunctions.net/info-service/getSituations", {lat: location.lat, lon: location.lng, radius: radius});
   };
 };
