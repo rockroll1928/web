3e0a780ebf08facbd5e46dcf9209ea9e45672888
hampus-rynebrant <hampus.rynebrant@sogeti.com>
2022-06-18 16:48:22 +0200

    added info overlay

---
+++ src/components/INFO/InfoOverlay.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Modal from './Modal.svelte';
+
+  let isOpenModal = false;
+
+  function openModal() {
+      isOpenModal = true;
+  }
+
+  function closeModal() {
+      isOpenModal = false;
+  }
+</script>
+
+<button on:click={openModal}>Open Modal</button>
+<Modal isOpenModal={isOpenModal} on:closeModal={closeModal} />
