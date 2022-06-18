f403cb26776015cf331f817750730cd5140dc223
Kevin Engstrom <kevin.engstrom@sogeti.com>
2022-06-18 16:44:28 +0200

    caaaarsd

--- src/components/STOPP/Drawer/Card.svelte
+++ src/components/STOPP/Drawer/Card.svelte
@@ -18,6 +18,10 @@ export let body;
   /* Add shadows to create the "card" effect */
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
+  height: 100px;
+  width: 150px;
+  margin:auto;
+  margin-top: auto;
 }
 
 /* On mouse-over, add a deeper shadow */
