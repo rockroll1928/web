55b9a70ee1fbc9caca6deddd340f597208c2e666
Maximilian Wendel <maximilian.wendel@sogeti.se>
2022-06-18 22:34:27 +0200

    Added lat, lon and radius to info service requests

--- src/services/InfoService.js
+++ src/services/InfoService.js
@@ -2,8 +2,8 @@ const Service = require("./Service");
 
 const InfoService = function (service = new Service()) {
 	const _service = service;
-  InfoService.prototype.getPinList = function (location) {
-    return _service.get("https://europe-west1-bootcamp222.cloudfunctions.net/info-service", location);
+  InfoService.prototype.getPinList = function (location, radius = 10 * 1000) {
+    return _service.get("https://europe-west1-bootcamp222.cloudfunctions.net/info-service", {...location, radius: radius});
   };
 };
 
