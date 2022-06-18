729c23a39cc72e2b4386b4edcff1f5888b664a16
Maximilian Wendel <maximilian.wendel@sogeti.se>
2022-06-18 16:19:05 +0200

    Fixed icon mapping utility name

--- src/utility/IconMapping.js
+++ src/utility/IconMapping.js
@@ -1,6 +1,6 @@
 const IconMapping = function() {};
 
-IconMapping = function(iconId) {
+IconMapping.prototype.translateIcon = function(iconId) {
 	switch (iconId) {
 		case 'roadAccident': return 'accident';
 		default: return 'undefined';
