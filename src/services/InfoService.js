6098f3114193614fa11ac45d755384eca8e99a86
Maximilian Wendel <maximilian.wendel@sogeti.se>
2022-06-18 23:10:45 +0200

    Updated info service to call the correct endpoint

--- src/services/InfoService.js
+++ src/services/InfoService.js
@@ -3,7 +3,7 @@ const Service = require("./Service");
 const InfoService = function (service = new Service()) {
 	const _service = service;
   InfoService.prototype.getPinList = function (location, radius = 10 * 1000) {
-    return _service.get("https://europe-west1-bootcamp222.cloudfunctions.net/info-service", {...location, radius: radius});
+    return _service.get("https://europe-west1-bootcamp222.cloudfunctions.net/info-service/getSituations", {lat: location.lat, lon: location.lng, radius: radius});
   };
 };
 
